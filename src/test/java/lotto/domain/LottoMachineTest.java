89e4c14c5b60155c379130ed1cc572ff4a27d618
yeo-li <parkseongyeol2110@gmail.com>
2025-11-03 03:34:27 +0900

    test(LottoMachineTest): 정상 생성 테스트 추가

---
+++ src/test/java/lotto/domain/LottoMachineTest.java
@@ -0,0 +1,34 @@
+package lotto.domain;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.List;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+
+class LottoMachineTest {
+
+    @Nested
+    @DisplayName("from() 테스트")
+    class FromTest {
+
+        @Test
+        @DisplayName("변환 성공")
+        void 변환_성공() {
+            // given
+            String winningNumbers = "10,30,20,11,45,1";
+            String bonusNumber = "3";
+
+            // when
+            LottoMachine actual = LottoMachine.from(winningNumbers, bonusNumber);
+
+            // then
+            List<Integer> expectedWinningNumbers = List.of(1, 10, 11, 20, 30, 45);
+            int expectedBonusNumber = 3;
+
+            assertThat(actual.getWinningNumbers().getNumbers()).isEqualTo(expectedWinningNumbers);
+            assertThat(actual.getBonusNumber()).isEqualTo(expectedBonusNumber);
+        }
+    }
+}
