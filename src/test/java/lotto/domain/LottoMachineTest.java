25a2204e100fb5ddffcd7ef3c9452d762bba06a0
yeo-li <parkseongyeol2110@gmail.com>
2025-11-03 05:37:00 +0900

    test(LottoMachineTest): 숫자 외 다른 문자가 포함되어 있는 경우 예외 발생 테스트 추가

--- src/test/java/lotto/domain/LottoMachineTest.java
+++ src/test/java/lotto/domain/LottoMachineTest.java
@@ -1,8 +1,10 @@
 package lotto.domain;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import java.util.List;
+import lotto.exception.LottoMachineErrorMessage;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
@@ -32,6 +34,18 @@ class LottoMachineTest {
             assertThat(actual.getBonusNumber()).isEqualTo(expectedBonusNumber);
         }
 
+        @Test
+        @DisplayName("숫자 외 다른 문자가 포함되어 있는 경우 예외 발생")
+        void 숫자_외_다른_문자가_포함되어_있는_경우_예외_발생() {
+            // given
+            WinningLotto winningNumbers = WinningLotto.from("10,30,20,11,45,1");
+            String bonusNumber = "3a";
+
+            // when & then
+            assertThatThrownBy(() -> LottoMachine.from(winningNumbers, bonusNumber))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage(LottoMachineErrorMessage.INVALID_CHARACTER.text());
+        }
 
     }
 }
