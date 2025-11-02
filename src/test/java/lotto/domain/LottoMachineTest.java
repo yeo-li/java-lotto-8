1ceea0f76bcaded08e4f644103196d7a3e6b77c0
yeo-li <parkseongyeol2110@gmail.com>
2025-11-03 06:30:45 +0900

    test(LottoMachineTest): 로또 발행 기능 정상 실행 테스트 코드 추가

--- src/test/java/lotto/domain/LottoMachineTest.java
+++ src/test/java/lotto/domain/LottoMachineTest.java
@@ -4,6 +4,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import java.util.List;
+import lotto.Lotto;
 import lotto.exception.LottoMachineErrorMessage;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
@@ -73,5 +74,27 @@ class LottoMachineTest {
                 .hasMessage(LottoMachineErrorMessage.DUPLICATED_NUMBER.text());
         }
 
+    }
+
+    @Nested
+    @DisplayName("issueLottoByAmount() 테스트")
+    class IssueLottoByAmountTest {
+
+        @Test
+        @DisplayName("정상 실행")
+        void 정상_실행() {
+            // given
+            WinningLotto winningNumbers = WinningLotto.from("10,30,20,11,45,1");
+            String bonusNumber = "3";
+            Money money = Money.from("16000");
+
+            // when
+            LottoMachine lottoMachine = LottoMachine.from(winningNumbers, bonusNumber);
+            List<Lotto> actual = lottoMachine.issueLottoByAmount(money);
+
+            // then
+            int expected = 16;
+            assertThat(actual.size()).isEqualTo(expected);
+        }
     }
 }
