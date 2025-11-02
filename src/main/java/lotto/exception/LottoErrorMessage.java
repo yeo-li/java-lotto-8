f77a76f69a2a43221f41fff2f88d159900f5d3b4
yeo-li <parkseongyeol2110@gmail.com>
2025-11-03 02:11:30 +0900

    refactor(LottoErrorMessage): 메세지 변경

--- src/main/java/lotto/exception/LottoErrorMessage.java
+++ src/main/java/lotto/exception/LottoErrorMessage.java
@@ -1,10 +1,10 @@
 package lotto.exception;
 
 public enum LottoErrorMessage {
-    INVALID_CHARACTER("당첨 번호에는 숫자와 컴마(,)만 포함될 수 있습니다."),
-    INVALID_COUNT("당첨 번호는 6개여야 합니다."),
-    OUT_OF_RANGE("당첨 번호는 1부터 45 사이의 숫자여야 합니다."),
-    DUPLICATED_NUMBER("당첨 번호에 중복된 숫자가 존재합니다.");
+    INVALID_CHARACTER("로또 번호에는 숫자와 컴마(,)만 포함될 수 있습니다."),
+    INVALID_COUNT("로또 번호는 6개여야 합니다."),
+    OUT_OF_RANGE("로또 번호는 1부터 45 사이의 숫자여야 합니다."),
+    DUPLICATED_NUMBER("로또 번호에 중복된 숫자가 존재합니다.");
 
     private final String message;
 
