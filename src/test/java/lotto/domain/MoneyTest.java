a655ae317fc8a4803e360cb3231f9fa2f2181171
yeo-li <parkseongyeol2110@gmail.com>
2025-11-03 00:13:53 +0900

    test(MoneyTest): 금액에 숫자를 제외한 다른 값이 있는 경우 예외 발생 테스트 추가

--- src/test/java/lotto/domain/MoneyTest.java
+++ src/test/java/lotto/domain/MoneyTest.java
@@ -29,8 +29,8 @@ class MoneyTest {
         }
 
         @Test
-        @DisplayName("금액이 음수인 경우")
-        void 금액이_음수인_경우() {
+        @DisplayName("금액이 음수인 경우 예외 발생")
+        void 금액이_음수인_경우_예외_발생() {
             // given
             String input = "-1000";
 
@@ -38,6 +38,18 @@ class MoneyTest {
             assertThatThrownBy(() -> Money.from(input))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage(MoneyErrorMessage.NEGATIVE_AMOUNT.text());
+        }
+
+        @Test
+        @DisplayName("금액에 숫자를 제외한 다른 값이 있는 경우 예외 발생")
+        void 금액에_숫자를_제외한_다른_값이_있는_경우_예외_발생() {
+            // given
+            String input = "1,000";
+
+            // when & then
+            assertThatThrownBy(() -> Money.from(input))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage(MoneyErrorMessage.NOT_NUMERIC.text());
         }
     }
 
