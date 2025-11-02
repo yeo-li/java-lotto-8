a3413752c3d0e3af4f749c844d3b3082dc1571f7
yeo-li <parkseongyeol2110@gmail.com>
2025-11-03 00:19:50 +0900

    test(MoneyTest): 음수인 경우 예외 상황 발생 테스트 삭제

--- src/test/java/lotto/domain/MoneyTest.java
+++ src/test/java/lotto/domain/MoneyTest.java
@@ -29,18 +29,6 @@ class MoneyTest {
         }
 
         @Test
-        @DisplayName("금액이 음수인 경우 예외 발생")
-        void 금액이_음수인_경우_예외_발생() {
-            // given
-            String input = "-1000";
-
-            // when & then
-            assertThatThrownBy(() -> Money.from(input))
-                .isInstanceOf(IllegalArgumentException.class)
-                .hasMessage(MoneyErrorMessage.NEGATIVE_AMOUNT.text());
-        }
-
-        @Test
         @DisplayName("금액에 숫자를 제외한 다른 값이 있는 경우 예외 발생")
         void 금액에_숫자를_제외한_다른_값이_있는_경우_예외_발생() {
             // given
