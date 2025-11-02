2c0bdef322bb5b8106c2e418929d41dedf172a5e
yeo-li <parkseongyeol2110@gmail.com>
2025-11-03 01:00:56 +0900

    feat(Parser): Parser 구현

---
+++ src/main/java/lotto/util/Parser.java
@@ -0,0 +1,15 @@
+package lotto.util;
+
+public class Parser {
+
+    public static String[] parseInput(String input) {
+
+        return removeAllSpaces(input).split(",", -1);
+    }
+
+    public static String removeAllSpaces(String input) {
+
+        return input.replaceAll(" ", "");
+    }
+}
+
