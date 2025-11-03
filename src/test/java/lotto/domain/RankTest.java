dc9109eea54a0d25f5791886f7a3c9333730b7f4
yeo-li <parkseongyeol2110@gmail.com>
2025-11-03 16:21:27 +0900

    test(RankTest): 5개 번호가 일치하고 보너스 번호가 일치하면 SECOND를 반환

--- src/test/java/lotto/domain/RankTest.java
+++ src/test/java/lotto/domain/RankTest.java
@@ -26,5 +26,20 @@ class RankTest {
             Rank expected = Rank.FIRST;
             assertThat(actual).isEqualTo(expected);
         }
+
+        @Test
+        @DisplayName("5개 번호가 일치하고 보너스 번호가 일치하면 SECOND를 반환")
+        void 다섯개_번호가_일치하고_보너스_번호가_일치하면_SECOND를_반환() {
+            // given
+            int count = 5;
+            boolean bonus = true;
+
+            // when
+            Rank actual = Rank.valueOf(count, bonus);
+
+            // then
+            Rank expected = Rank.SECOND;
+            assertThat(actual).isEqualTo(expected);
+        }
     }
 }
