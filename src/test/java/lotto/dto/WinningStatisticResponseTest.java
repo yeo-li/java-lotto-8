2345e50b55ae749448a019f4a61f75c937897bc1
yeo-li <parkseongyeol2110@gmail.com>
2025-11-03 20:02:22 +0900

    test(WinningStatisticResponseTest): 로또 통계 Map을 WinningStatisticResponse로 변환 테스트 추가

---
+++ src/test/java/lotto/dto/WinningStatisticResponseTest.java
@@ -0,0 +1,32 @@
+package lotto.dto;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+import java.util.Map;
+import lotto.domain.Rank;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+
+class WinningStatisticResponseTest {
+
+    @Nested
+    @DisplayName("from() 테스트")
+    class FromTest {
+
+        @Test
+        @DisplayName("로또 통계 Map을 WinningStatisticResponse로 변환")
+        void from_정상_변환() {
+            Map<Rank, Integer> statistic = Map.of(
+                Rank.FIRST, 1,
+                Rank.THIRD, 2
+            );
+
+            WinningStatisticResponse response = WinningStatisticResponse.from(statistic);
+
+            assertThat(response.firstCount()).isEqualTo(1);
+            assertThat(response.thirdCount()).isEqualTo(2);
+            assertThat(response.fourthCount()).isEqualTo(0); // 없는 값은 0
+        }
+    }
+}
