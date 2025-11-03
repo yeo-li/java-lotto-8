9b0a3d3f4440d4c25d8437ea2a075a16c0dfc7e6
yeo-li <parkseongyeol2110@gmail.com>
2025-11-03 16:20:10 +0900

    feat(Rank): 6개 번호가 일치하고 보너스 번호가 일치하지 않으면 FIRST를 반환 로직 구현

---
+++ src/main/java/lotto/domain/Rank.java
@@ -0,0 +1,28 @@
+package lotto.domain;
+
+public enum Rank {
+    FIRST(6, false, 2_000_000_000),
+    SECOND(5, true, 30_000_000),
+    THIRD(5, false, 1_500_000),
+    FOURTH(4, false, 50_000),
+    FIFTH(3, false, 5_000),
+    MISS(0, false, 0);
+
+    private final int matchCount;
+    private final boolean matchBonus;
+    private final int prize;
+
+    Rank(int matchCount, boolean matchBonus, int prize) {
+        this.matchCount = matchCount;
+        this.matchBonus = matchBonus;
+        this.prize = prize;
+    }
+
+    public int prize() {
+        return prize;
+    }
+
+    public static Rank valueOf(int count, boolean bonus) {
+        return Rank.FIRST;
+    }
+}
