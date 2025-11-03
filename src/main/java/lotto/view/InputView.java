49cdcd7d33e3bf764f8bff89814782da2ba1b7f2
yeo-li <parkseongyeol2110@gmail.com>
2025-11-03 21:12:18 +0900

    refactor(InputView): 메세지에 엔터 추가

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -4,9 +4,9 @@ import camp.nextstep.edu.missionutils.Console;
 
 public class InputView {
 
-    private static final String PROMPT_PURCHASE_AMOUNT_MESSAGE = "구입금액을 입력해 주세요.";
-    private static final String PROMPT_WINNING_NUMBERS_MESSAGE = "당첨 번호를 입력해 주세요.";
-    private static final String PROMPT_BONUS_NUMBER_MESSAGE = "보너스 번호를 입력해 주세요.";
+    private static final String PROMPT_PURCHASE_AMOUNT_MESSAGE = "\n구입금액을 입력해 주세요.";
+    private static final String PROMPT_WINNING_NUMBERS_MESSAGE = "\n당첨 번호를 입력해 주세요.";
+    private static final String PROMPT_BONUS_NUMBER_MESSAGE = "\n보너스 번호를 입력해 주세요.";
 
     public String inputPurchaseAmount() {
         System.out.println(PROMPT_PURCHASE_AMOUNT_MESSAGE);
