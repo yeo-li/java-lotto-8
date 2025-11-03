bd7ddb1fadfcd0bf10b124d51e5ad6b944effc25
yeo-li <parkseongyeol2110@gmail.com>
2025-11-03 20:45:34 +0900

    test(LottoResponseTest): List<LottoResponse> 변환 테스트 추가

--- src/test/java/lotto/dto/LottoResponseTest.java
+++ src/test/java/lotto/dto/LottoResponseTest.java
@@ -2,6 +2,7 @@ package lotto.dto;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
+import java.util.List;
 import lotto.domain.Lotto;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
@@ -25,5 +26,23 @@ class LottoResponseTest {
             // then
             assertThat(response.formattedNumbers()).isEqualTo("[1, 2, 3, 4, 5, 6]");
         }
+    }
+
+    @Test
+    @DisplayName("Lotto 리스트를 LottoResponse 리스트로 변환하면 각각 포맷된 문자열로 저장")
+    void from_Lotto_리스트를_LottoResponse_리스트로_변환하면_포맷된_문자열이_저장() {
+        // given
+        List<Lotto> lottos = List.of(
+            Lotto.from("1,2,3,4,5,6"),
+            Lotto.from("7,8,9,10,11,12")
+        );
+
+        // when
+        List<LottoResponse> responses = LottoResponse.from(lottos);
+
+        // then
+        assertThat(responses.size()).isEqualTo(2);
+        assertThat(responses.get(0).formattedNumbers()).isEqualTo("[1, 2, 3, 4, 5, 6]");
+        assertThat(responses.get(1).formattedNumbers()).isEqualTo("[7, 8, 9, 10, 11, 12]");
     }
 }
